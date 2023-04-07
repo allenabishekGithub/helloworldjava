16fc4de8b036a34324c8964c24aedb68ffdf6660
allenabishekGithub <111240053+allenabishekGithub@users.noreply.github.com>
2023-04-07 18:44:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Feb 19th 2023 - 09:05PM - Allen Abishek, this is edit for the TestSlave Node");
+    System.out.println("Hello World - Feb 19th 2023 - 09:05PM - Allen Abishek, this is edit for the Pipeline Project");
     }
 }
